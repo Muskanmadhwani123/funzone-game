1e7eb6ae5a3d4da2d8e392c07e6afd9e71acf878
muskanmadhwani123 <madhwanimuskan123@gmail.com>
2026-01-11 13:54:48 +0530

    Login page working with form data

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("welcome.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
+    if request.method=="POST":
+        username=request.form["Username"]
+        password = request.form["password"]
+
+        print("Username: ", username)
+        print("Password: ",password)
+             
     return render_template("login.html")
 
 if __name__== "__main__":
